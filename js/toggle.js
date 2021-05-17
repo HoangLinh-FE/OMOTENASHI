8364ca31d06e46dcff87fc7e43319b1c9514655f
HoangLinh-FE <58255319+HoangLinh-FE@users.noreply.github.com>
2021-05-18 02:35:20 +0700

    update for customer

--- js/toggle.js
+++ js/toggle.js
@@ -5,10 +5,14 @@
     
     $(".educate__content").hide();
     $(".educate__content:first").show();
+    
     $(".educate__item").click(function(){
       var myparent = $(this).parent();
       $(".educate__content", myparent).each(function(){
-          $(this).toggle();      
+          if($(this).show){
+            $(".educate__content").hide();
+          }
+          $(this).toggle(500); 
       });
     });
   });
